100651da2540e7a14959053b0664b9658baefb60
Ho Ill Jung <hoil0221@tmon.co.kr>
2018-08-29 23:14:49 +0900

    redux dev tool 추가

--- src/configureStore.js
+++ src/configureStore.js
@@ -5,13 +5,20 @@ import { loadState, saveState } from './localStorage';
 
 const configureStore = () => {
   const persistedState = loadState();
-  const store = createStore(todoApp, persistedState);
+  const store = createStore(
+    todoApp,
+    persistedState,
+    window.__REDUX_DEVTOOLS_EXTENSION__ &&
+      window.__REDUX_DEVTOOLS_EXTENSION__()
+  );
 
-  store.subscribe(throttle(() => {
-    saveState({
-      todos: store.getState().todos,
-    });
-  }, 1000));
+  store.subscribe(
+    throttle(() => {
+      saveState({
+        todos: store.getState().todos,
+      });
+    }, 1000)
+  );
 
   return store;
 };
